79de17c4efc329923cc94a3de13b240238dea14c
Vovashprot <vova.protasenko@gmail.com>
2023-06-18 19:15:15 +0300

    Now to test

--- src/main/java/com/Calculator/Calculator/Controller/CalculatorController.java
+++ src/main/java/com/Calculator/Calculator/Controller/CalculatorController.java
@@ -7,26 +7,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Calculator")
+@RequestMapping("/calculator")
 public class CalculatorController {
     public final CalculatorService service;
     public CalculatorController(CalculatorService service){
         this.service = service;
     }
+    @RequestMapping("")
+    public String greetings(){
+        return "Добро пожаловать в калькулятор";
+    }
     @GetMapping("/plus")
-    public Integer calculatorPlus(@RequestParam("input1") Integer input1, @RequestParam("input2") Integer input2){
+    public Integer calculatorPlus(@RequestParam("num1") Integer input1, @RequestParam("num2") Integer input2){
         return service.plus(input1,input2);
     }
     @GetMapping("/minus")
-    public Integer calculatorMinus(@RequestParam("input1") Integer input1, @RequestParam("input2") Integer input2){
+    public Integer calculatorMinus(@RequestParam("num1") Integer input1, @RequestParam("num2") Integer input2){
         return service.minus(input1,input2);
     }
     @GetMapping("/multiply")
-    public Integer calculatorMultiply(@RequestParam("input1") Integer input1, @RequestParam("input2") Integer input2){
+    public Integer calculatorMultiply(@RequestParam("num1") Integer input1, @RequestParam("num2") Integer input2){
         return service.multiply(input1,input2);
     }
     @GetMapping("/divide")
-    public Integer calculatorDivide(@RequestParam("input1") Integer input1, @RequestParam("input2") Integer input2){
+    public Integer calculatorDivide(@RequestParam("num1") Integer input1, @RequestParam("num2") Integer input2){
         return service.divide(input1,input2);
     }
 }
--- src/main/java/com/Calculator/Calculator/Service/CalculatorService.java
+++ src/main/java/com/Calculator/Calculator/Service/CalculatorService.java
@@ -1,5 +1,8 @@
 package com.Calculator.Calculator.Service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorService {
     public Integer plus(Integer input1, Integer input2){
         int result = input1 + input2;
