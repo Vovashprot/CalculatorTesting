d8f8234893b584c5e7e18f3f79a3f67dcee5665c
Vovashprot <vova.protasenko@gmail.com>
2023-06-18 18:52:33 +0300

    1

--- src/main/java/com/Calculator/Calculator/Controller/CalculatorController.java
+++ src/main/java/com/Calculator/Calculator/Controller/CalculatorController.java
@@ -1,9 +1,32 @@
 package com.Calculator.Calculator.Controller;
 
+import com.Calculator.Calculator.Service.CalculatorService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Calculator")
 public class CalculatorController {
+    public final CalculatorService service;
+    public CalculatorController(CalculatorService service){
+        this.service = service;
+    }
+    @GetMapping("/plus")
+    public Integer calculatorPlus(@RequestParam("input1") Integer input1, @RequestParam("input2") Integer input2){
+        return service.plus(input1,input2);
+    }
+    @GetMapping("/minus")
+    public Integer calculatorMinus(@RequestParam("input1") Integer input1, @RequestParam("input2") Integer input2){
+        return service.minus(input1,input2);
+    }
+    @GetMapping("/multiply")
+    public Integer calculatorMultiply(@RequestParam("input1") Integer input1, @RequestParam("input2") Integer input2){
+        return service.multiply(input1,input2);
+    }
+    @GetMapping("/divide")
+    public Integer calculatorDivide(@RequestParam("input1") Integer input1, @RequestParam("input2") Integer input2){
+        return service.divide(input1,input2);
+    }
 }
--- src/main/java/com/Calculator/Calculator/Service/CalculatorService.java
+++ src/main/java/com/Calculator/Calculator/Service/CalculatorService.java
@@ -1,4 +1,20 @@
 package com.Calculator.Calculator.Service;
 
 public class CalculatorService {
+    public Integer plus(Integer input1, Integer input2){
+        int result = input1 + input2;
+        return result;
+    }
+    public Integer minus(Integer input1, Integer input2){
+        int result = input1 - input2;
+        return result;
+    }
+    public Integer multiply(Integer input1, Integer input2){
+        int result = input1 * input2;
+        return result;
+    }
+    public Integer divide(Integer input1, Integer input2){
+        int result = input1 / input2;
+        return result;
+    }
 }
